1e48f977dc19adf40036d3be65991d3dafc23bac
Angel Garza <aagarza@proton.me>
2024-02-19 21:27:23 -0500

    added my commandline args

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="--share --listen --no-download-sd-model --enable-insecure-extension-access --opt-sdp-no-mem-attention"
 
 # python3 executable
 #python_cmd="python3"
